05d163fc9238d40edb1a158c988f97d8c521e22b
rayrayyrayyy <allssndr.ray@gmail.com>
2023-05-09 01:30:42 +0800

    opening a window

--- mini calculator.py
+++ mini calculator.py
@@ -11,9 +11,14 @@ from tkinter import ttk
 # define a function for division of numbers
 
 # open a window using tkinter
+root_window = Tk()
+root_window.title("MINI CALCULATOR")
+root_window.geometry("500x500")
+
 # create buttons
-# define function to open another window based on the button that the user clicked
+# define a function to open another window based on the button that the user clicked
 # create entry 
 # create exit button
 
 # mainloop
+root_window.mainloop()
