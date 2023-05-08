d4e403b5561e4a4627dc3883432e8f73409991f3
rayrayyrayyy <allssndr.ray@gmail.com>
2023-05-09 01:20:59 +0800

    pseudocode

--- mini calculator.py
+++ mini calculator.py
@@ -1,2 +1,16 @@
 # Ray Allessandra D. Pacis | BSCPE 1-5
 
+# import tkinter library
+
+# define a function for addition of numbers
+# define a function for subtraction of numbers
+# define a function for multiplication of numbers
+# define a function for division of numbers
+
+# open a window using tkinter
+# create buttons
+# define function to open another window based on the button that the user clicked
+# create entry 
+# create exit button
+
+# mainloop
