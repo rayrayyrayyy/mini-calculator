a1b3dad768961229434513747ebd9c6791c7813d
rayrayyrayyy <allssndr.ray@gmail.com>
2023-05-09 22:55:47 +0800

    try

--- mini calculator.py
+++ mini calculator.py
@@ -32,13 +32,15 @@ def divide():
         total = float(first_num.get()) / float(second_num.get()) # variable first_num and second_num to be follow
     except:
         messagebox.showerror("Showerror", "ERROR! Invalid input!\n Please enter numbers only.")
-        
+
 # open a window using tkinter
 root_window = Tk()
 root_window.title("MINI CALCULATOR")
 root_window.geometry("500x500")
+root_window.config(bg = "brown")
 
 # create buttons
+
 # define a function to open another window based on the button that the user clicked
 # create entry 
 # create exit button
