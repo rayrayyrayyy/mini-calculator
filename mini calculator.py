c580ec5dd891810b89a98c0b200af91008250fb4
rayrayyrayyy <allssndr.ray@gmail.com>
2023-05-10 22:13:33 +0800

    add label for result

--- mini calculator.py
+++ mini calculator.py
@@ -85,6 +85,10 @@ def subtract():
     note1 = Label(value_window, text = "Enter second number:", bg = "cyan", font=("Times", 15), justify = CENTER)
     note1.place(x=10, y=40)
 
+    # add label for result
+    result = Label(value_window, text = "Result\t= ", bg = "cyan", font=("Times", 20), justify = CENTER)
+    result.place(x=50, y=130)
+
     try:
         total = float(first_num.get()) - float(second_num.get()) 
     except:
