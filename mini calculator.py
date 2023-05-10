b575ee6730fd75db4d67e23875a6657e67884cb9
rayrayyrayyy <allssndr.ray@gmail.com>
2023-05-10 22:04:40 +0800

    define clear

--- mini calculator.py
+++ mini calculator.py
@@ -37,6 +37,11 @@ def addition():
     back_button = Button(value_window, text = "BACK", width = 20, command = value_window.destroy)
     back_button.place(x=225, y=200)
 
+    # define clear
+    def clear():
+        first_num.delete(0, END)
+        second_num.delete(0, END)
+
     # add clear button
     clear_button = Button(value_window, text = "CLEAR", width = 20, command = clear)
     clear_button.place(x=50, y=200)
