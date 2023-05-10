ca6bbeb6167dd8fc00722861af123fabd3e7374d
rayrayyrayyy <allssndr.ray@gmail.com>
2023-05-10 18:55:13 +0800

    change func name, move var entry

--- mini calculator.py
+++ mini calculator.py
@@ -29,12 +29,11 @@ def addition():
     note1 = Label(value_window, text = "Enter second number:", font=("Times", 15), justify = CENTER)
     note1.place(x=10, y=40)
 
+    def add():
+            entry.config(text = v)
+
     entry = Label(value_window, text = "", font=("Times", 15), justify = CENTER)
     entry.place(x=200, y=80)
-
-    def final_value():
-            v = total.get()
-            entry.config(text = v)
 
     result_button = Button(value_window, text = "SHOW RESULT", width = 20, command = final_value)
     result_button.place(x=250, y=80)
